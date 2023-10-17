4523790d0c8d5a811fc2e09f57916c9d7ceb7d57
Ronak fiza <109734453+ronakfiza@users.noreply.github.com>
2023-10-17 21:59:43 +0530

    Revert "add extra"
    This reverts commit fb9ed007a41a4c7dd2aba7e981b6fba8e9f1a594.

--- resources/views/single_task.blade.php
+++ resources/views/single_task.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Single Task Perform</title>
+    <title>Document</title>
     <link rel="stylesheet" href="assets/addtask.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
